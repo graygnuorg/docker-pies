33b097530413103eba2e42b4b43e33e87a2ff3a9
Sergey Poznyakoff <gray@gnu.org>
2021-06-23 17:05:56 +0200

    Rewrite build system

---
+++ debian/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+: ${IMAGENAME:?}
+: ${PLATFORM:=$(basename $(pwd))}
+: ${BUILD:?}
+set -x    
+docker build -t $IMAGENAME ${NOCACHE:+--no-cache} \
+          --build-arg OSVERSION=$OSVERSION \
+          --build-arg PIES_TAG=$PIES_TAG \
+          --build-arg XENV_TAG=$XENV_TAG \
+	  -f ${PLATFORM}/Dockerfile \
+          tree
