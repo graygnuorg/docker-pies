7a4b9d04b9c679c6cb8d931c997253bbfc2b1c9e
Sergey Poznyakoff <gray@gnu.org>
2021-07-07 15:06:09 +0200

    Label the images
    * Build: Export all variables from buildtab entry.
    Determine and export PIES_VERSION and XENV_VERSION.
    * build.sh: Add --build-arg definitions.
    * alpine/Dockerfile: Add LABELs
    * debian/Dockerfile: Likewise.

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 : ${IMAGENAME:?}
 : ${PLATFORM:?}
 : ${BUILD:?}
-set -x    
+: ${VERSION:?}
+set -x
 docker build -t $IMAGENAME ${NOCACHE:+--no-cache} \
+          --build-arg VERSION=$VERSION \
           --build-arg OSVERSION=$OSVERSION \
           --build-arg PIES_TAG=$PIES_TAG \
+	  --build-arg PIES_VERSION=${PIES_VERSION:-$PIES_TAG} \
           --build-arg XENV_TAG=$XENV_TAG \
+	  --build-arg XENV_VERSION=${XENV_VERSION:-$XENV_TAG} \
+	  --build-arg CREATEDTIME=$(date --utc +'%Y-%m-%dT%H:%M:%SZ') \
 	  -f ${PLATFORM}/Dockerfile \
           tree
